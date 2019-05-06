f453d7cf861863a69b73e058f2893557fa73501f
M639 <43021768+M639@users.noreply.github.com>
2019-05-06 15:47:28 +0800

    add

---
+++ gdmaps/src/main/java/test/mfy/com/gdmaps/KConstant.java
@@ -0,0 +1,10 @@
+package test.mfy.com.gdmaps;
+
+public class KConstant {
+    //经度
+    public static double longitude = 0;
+    //纬度
+    public static double latitude = 0;
+    //城市
+    public static String CITY;
+}
